b4c4c31a53fe2cb1fe735c759588014eb5640a51
Scott Gorsky <sgorsky@gmail.com>
2021-08-28 14:32:41 -0500

    submit button

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,16 +2,27 @@ import React, {useState} from 'react'
 
 function TodoForm() {
     const [input, setInputs] = useState('')
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000)
+            
+        })
+    }
+
     return (
         <div>
-           <form className="todo-form">
+           <form className="todo-form" onSubmit={handleSubmit}>
                <input 
                 type="text" 
                 placeholder="Add a Todo" 
                 value={input} 
                 name="text" 
                 className="todo-input">
-                   </input></form> 
+                   </input>
+                <button className="todo-button">Add Todo</button>
+                   </form> 
         </div>
     )
 }
